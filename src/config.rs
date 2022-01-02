b532f0c6ed40e44103e3b92fc4d09833237dd566
Luis Tavares <github@sheeponline.net>
2022-01-02 10:31:04 +0000

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,216 @@
+use dirs;
+
+use std::path::{ Path, PathBuf };
+
+use std::fs;
+use std::fs::{ File, Metadata };
+use std::io::{ BufReader, BufRead, Read, BufWriter, Write, Error };
+
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[cfg(target_os = "linux")]
+use std::os::unix::fs::{ PermissionsExt, MetadataExt };
+
+#[derive(Serialize, Deserialize)]
+pub struct Settings {
+  pub set_default: i32,
+  pub homepage: String,
+  pub host_info: bool,
+  pub icons_per_row: i32,
+  pub icon_spacing: i32
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BrowserSettings {
+  pub title: String,
+  pub executable: String,
+  pub arguments: String,
+  pub icon: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Configuration {
+  pub settings: Settings,
+  pub browsers_list: Vec<BrowserSettings>
+}
+
+pub fn get_configuration() -> Configuration {
+  let configuration:Configuration;
+  let configuration_root_directory:String;
+  let home_dir_path:Option<PathBuf>;
+  let mut config_directory_buf:PathBuf;
+  let mut config_file_buf:PathBuf;
+
+  let config_directory_path:&Path;
+  let config_file_path:&Path;
+
+  // Check user home directory, this should always exist.
+  home_dir_path = dirs::home_dir();
+  if home_dir_path.is_none() {
+    //TODO: Abort with error
+  }
+
+  // Create PathBuf objects for configuration directory and file.
+  config_directory_buf = home_dir_path.unwrap();
+  config_directory_buf.push(".browsewith/");
+  config_file_buf = config_directory_buf.clone();
+  config_file_buf.push(".brosewith/"); //BUG: config_directory_buf already has '.browsewith' but its lost when clonned to config_file_buf
+  config_file_buf.set_file_name("config.json");
+
+  // Transform the PathBuf objects to Path, so we can do directory/file checks and other operations
+  config_directory_path = config_directory_buf.as_path();
+  config_file_path = config_file_buf.as_path();
+
+  configuration_root_directory = format!("config_directory_buf: {:?}\nconfig_file_buf: {:?}\nconfig_directory_path: {:?}", config_directory_buf, config_file_buf, config_directory_path );
+  println!("{}",configuration_root_directory);
+
+  // Create configuration directory and file if required
+  if !config_directory_path.is_dir() {
+    println!("Configuration directory not found");
+    fs::create_dir(config_directory_path);
+  }
+  if !config_file_path.is_file() {
+    println!("Configuration file not found");
+    create_configuration_file(config_file_path);
+  }
+
+  configuration = load_configuration(&config_file_path);
+  return configuration;
+}
+
+fn create_configuration_file(config_full_path:&Path) {
+  let default_configuration:Configuration;
+  default_configuration = get_default_settings();
+  save_configuration(config_full_path, &default_configuration);
+}
+
+fn get_default_settings() -> Configuration {
+  let installed_browsers:Vec<BrowserSettings> = get_browser_list();
+
+  let default_settings = Configuration {
+    settings: Settings {
+      set_default: 1,
+      homepage: "about:blank".to_string(),
+      host_info: true,
+      icons_per_row: 3,
+      icon_spacing: 5
+    },
+    browsers_list: installed_browsers
+  };
+  return default_settings;
+}
+
+fn get_browser_list() -> Vec<BrowserSettings> {
+  let mut executable_buf:PathBuf;
+  let mut executable_path:&Path;
+  let mut icon_buf:PathBuf;
+  let mut icon_path:&Path;
+
+  let mut browser_list:Vec<BrowserSettings> = [
+    BrowserSettings { title: "Brave".to_string(), executable: "brave-browser".to_string(), arguments: "".to_string(), icon: "brave-browser.png".to_string() },
+    BrowserSettings { title: "Brave Incognito".to_string(), executable: "brave-browser".to_string(), arguments: "--incognito".to_string(), icon: "brave-browser.png".to_string() },
+    BrowserSettings { title: "Brave TOR".to_string(), executable: "brave-browser".to_string(), arguments: "".to_string(), icon: "brave-browser.png".to_string() },
+    BrowserSettings { title: "Edge".to_string(), executable: "Microsoft\\Edge\\Application\\msedge.exe".to_string(), arguments: "--tor".to_string(), icon: "".to_string() },
+    BrowserSettings { title: "Edge InPrivate".to_string(), executable: "Microsoft\\Edge\\Application\\msedge.exe".to_string(), arguments: "--tor".to_string(), icon: "".to_string() },
+    BrowserSettings { title: "Firefox".to_string(), executable: "firefox".to_string(), arguments: "-new-tab".to_string(), icon: "firefox.png".to_string() },
+    BrowserSettings { title: "Firefox Private".to_string(), executable: "firefox".to_string(), arguments: "-private-window".to_string(), icon: "firefox.png".to_string() },
+    BrowserSettings { title: "Google".to_string(), executable: "chrome".to_string(), arguments: "--tor".to_string(), icon: "google-chrome.png".to_string() },
+    BrowserSettings { title: "Google Incognito".to_string(), executable: "chrome".to_string(), arguments: "--incognito".to_string(), icon: "google-chrome.png".to_string() },
+    BrowserSettings { title: "Chromium".to_string(), executable: "chromium-browser".to_string(), arguments: "--tor".to_string(), icon: "chromium-browser.png".to_string() },
+    BrowserSettings { title: "Chromium Incognito".to_string(), executable: "chromium-browser".to_string(), arguments: "--incognito".to_string(), icon: "chromium-browser.png".to_string() },
+    // { title: "".to_string(), executable: "".to_string(), arguments: "--tor".to_string(), icon: "".to_string() },
+  ].to_vec();
+  
+  let mut browsers_found:Vec<BrowserSettings> = [].to_vec();
+  let mut browsers_settings:BrowserSettings;
+
+  let well_known_bin = [
+    "/usr/bin",
+    "/usr/local/bin",
+    "/bin"
+  ];
+  let well_known_icon = [
+    "/usr/share/icons/hicolor/32x32/apps",
+    "/usr/share/icons/hicolor/64x64/apps",
+    "/usr/share/icons/Papirus/32x32/apps",
+    "/usr/share/icons/Papirus/64x64/apps",
+  ];
+
+  // let metadata:Result<Metadata>;
+  for browser in &mut browser_list {
+    for bin in well_known_bin {
+      executable_buf = Path::new(bin).join(&browser.executable);
+      executable_path = executable_buf.as_path();
+
+      if is_file_executable(&executable_path) {
+        browsers_settings = browser.clone();
+        
+        match executable_path.to_str() {
+          None => browsers_settings.executable = "".to_string(),
+          Some(f) => {
+            browsers_settings.executable = f.to_string();
+          }
+        }
+
+        for icon in well_known_icon {
+          icon_buf = Path::new(icon).join(&browser.icon);
+          icon_path = icon_buf.as_path();
+          println!("icon path {:?}\t{}", icon_path, icon);
+          if icon_path.exists() {
+            match icon_path.to_str() {
+              None => browsers_settings.icon = "".to_string(),
+              Some(i) => {
+                browsers_settings.icon = i.to_string();
+              }
+            }
+            break;
+          }
+        }
+
+        browsers_found.push(browsers_settings.clone());
+        break;
+      }
+    }
+  }
+
+  return browsers_found;
+}
+
+fn load_configuration(file_path:&Path) -> Configuration {
+  let reader:BufReader<File>;
+  let file_handle:File;
+  let configuration:Configuration;
+
+  file_handle = File::open(file_path).unwrap();
+  reader = BufReader::new(file_handle);
+  configuration = serde_json::from_reader(reader).unwrap();
+  println!("Configuration loaded");
+  return configuration;
+}
+
+fn save_configuration(file_path:&Path, data:&Configuration) {
+  let writer:BufWriter<File>;
+  let file_handle:File;
+
+  file_handle = File::create(file_path).unwrap();
+  writer = BufWriter::new(file_handle);
+  serde_json::to_writer_pretty(writer, &data);
+}
+
+fn is_file_executable(path:&Path) -> bool {
+  let file:File;
+  let file_mode:u32;
+  let file_metadata:&MetadataExt;
+
+  if ! path.exists() { return false; }
+
+  if cfg!(unix) {
+    // TODO: Check if user has exec permissions on the file,
+    // for now just assume that he has
+    return true;
+  }
+
+  return false;
+}
+
---
+++ src/main.rs
@@ -0,0 +1,153 @@
+use gtk::prelude::*;
+use gtk::{ Application, ApplicationWindow, Button, Image, Box, Orientation, Align, PositionType, Label, WindowPosition };
+use gtk::gdk_pixbuf::{ Pixbuf, InterpType };
+
+// Configuration module
+mod config;
+
+#[derive(Clone, Copy)]
+struct ButtonMargins {
+  left: i32,
+  top: i32,
+  right: i32,
+  bottom: i32
+}
+
+fn main() {
+  let configuration:config::Configuration;
+
+  configuration = config::get_configuration();
+  show_application_window(configuration);
+}
+
+fn show_application_window(configuration:config::Configuration) {
+  let application = Application::builder()
+  .application_id("com.sheep.browsewith")
+  .build();
+
+  application.connect_activate(move |app| {
+    let window_box:Box = Box::new(Orientation::Vertical, 0);
+    let icons_box:Box = Box::new(Orientation::Vertical, 0);
+    let hostinfo_box:Box;
+    let mut icons_row:Box = Box::new(Orientation::Horizontal, 0);
+    let mut icon_counter:i32 = 1;
+    let icons_per_row:i32 = configuration.settings.icons_per_row;
+    let icon_spacing:i32 = configuration.settings.icon_spacing;
+    let mut icon_spacing_top:i32 = configuration.settings.icon_spacing;
+
+    let button_margin_default:ButtonMargins = ButtonMargins { left: icon_spacing, top: icon_spacing_top, right: 0, bottom: icon_spacing };
+    let button_margin_last:ButtonMargins = ButtonMargins { left: icon_spacing, top: icon_spacing, right: icon_spacing, bottom: icon_spacing };
+
+    let window = ApplicationWindow::builder()
+      .application(app)
+      .title("BrowseWith")
+      .default_width(180+icon_spacing*2)
+      .default_height(70)
+      .window_position(WindowPosition::Center)
+      .build();
+
+    // Add all browsers as icons to a Box widget, creating a new child Box widget
+    // for every 'icons_per_row' browsers
+    icons_box.add(&icons_row);
+    for browser in configuration.browsers_list.clone() {
+      if icon_counter % icons_per_row == 0 {
+        button_with_image(&browser, &icons_row, button_margin_last);
+        icons_row = Box::new(Orientation::Horizontal, 0);
+        icons_box.add(&icons_row);
+        icon_spacing_top = 0;
+      } else {
+        button_with_image(&browser, &icons_row, button_margin_default);
+      }
+      icon_counter = icon_counter + 1;
+    }
+    window_box.add(&icons_box);
+
+    // Check if we need to add taget URL host information
+    if configuration.settings.host_info {
+      hostinfo_box = diplay_host_info();
+      window_box.add(&hostinfo_box);
+    }
+
+    // Display main windows with all the components
+    window.add(&window_box);
+    window.show_all();
+  
+  });
+
+  application.run();
+}
+
+fn button_with_image(browser_settings:&config::BrowserSettings, box_object:&Box, margins:ButtonMargins) {
+  let image:Image;
+  let button:Button;
+  let button_box:Box;
+  let mut image_pixbuf:Pixbuf;
+
+  let bs = config::BrowserSettings {
+    title: (&browser_settings.title).to_string(),
+    executable: String::from(""),
+    arguments: String::from(""),
+    icon: (&browser_settings.icon).to_string()
+  };
+  let b = bs.clone();
+
+  // Create a button with image and label, assigning a function for when clicked
+
+  image_pixbuf = Pixbuf::from_file(browser_settings.icon.clone()).unwrap();
+  if image_pixbuf.width() != 32 && image_pixbuf.height() != 32 {
+    image_pixbuf = image_pixbuf.scale_simple(32, 32, InterpType::Bilinear ).unwrap();
+  }
+  image = Image::from_pixbuf(Some(&image_pixbuf));
+
+  button = Button::builder()
+    .width_request(180).height_request(60)
+    .image(&image).always_show_image(true).image_position(PositionType::Left)
+    .label(&browser_settings.title)
+    .build();
+  button.connect_clicked(move |_| {button_clicked(&b)});
+
+  //
+  button_box = Box::builder()
+    .orientation(Orientation::Horizontal)
+    .spacing(0)
+    .margin_start(margins.left)
+    .margin_top(margins.top)
+    .margin_end(margins.right)
+    .margin_bottom(margins.bottom)
+    .build();
+  button_box.add(&button);
+
+  // Add to the main window
+  box_object.add(&button_box);
+}
+
+fn button_clicked<'a>(browser_settigns:&'a config::BrowserSettings ) {
+  eprintln!("{:?}", &browser_settigns.icon);
+}
+
+fn diplay_host_info() -> Box {
+  let box_object:Box;
+  let label_url:Label;
+  let label_page_title:Label;
+  let label_ssl_status:Label;
+  let label_response:Label;
+
+  label_url = Label::builder().label("URL:").halign(Align::Start).build();
+  label_page_title = Label::builder().label("Page title:").halign(Align::Start).build();
+  label_ssl_status = Label::builder().label("SSL Statue:").halign(Align::Start).build();
+  label_response = Label::builder().label("Response:").halign(Align::Start).build();
+
+  box_object = Box::builder()
+    .orientation(Orientation::Vertical)
+    .spacing(0)
+    .margin_start(5)
+    .margin_bottom(5)
+    .halign(Align::Start)
+    .build();
+  box_object.add(&label_url);
+  box_object.add(&label_page_title);
+  box_object.add(&label_ssl_status);
+  box_object.add(&label_response);
+
+  return box_object;
+}
